8dc47b922935b260c1c8f092821fd4cced0ee9fa
Sergey-Ivanovich <ericnoata@gmail.com>
2024-02-24 12:45:15 +0000

    added clock and date functionality to 2 cities on main page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
+function updateTime() {
+  // ANKARA
+  let ankara = document.querySelector("#ankara");
+  let shownAnkaraDate = ankara.querySelector("p");
+  let shownAnkaraTime = ankara.querySelector(".time");
+  let ankaraDate = moment().tz("Europe/Istanbul");
+  shownAnkaraDate.innerHTML = ankaraDate.format("MMMM Do YYYY");
+  shownAnkaraTime.innerHTML = ankaraDate.format(
+    "hh:mm:ss [<small>]A[</small>]"
+  );
 
+  // ABU-DHABI
+  let abuDhabi = document.querySelector("#abu-dhabi");
+  let shownAbuDhabiDate = abuDhabi.querySelector("p");
+  let shownAbuDhabiTime = abuDhabi.querySelector(".time");
+  let abuDhabiDate = moment().tz("Asia/Dubai");
+  shownAbuDhabiDate.innerHTML = abuDhabiDate.format("MMMM Do YYYY");
+  shownAbuDhabiTime.innerHTML = abuDhabiDate.format(
+    "hh:mm:ss [<small>]A[</small>]"
+  );
+}
+
+updateTime();
+setInterval(updateTime, 900);
